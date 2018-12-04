b81d125f75fbc181bf1f14bcc639a4399ac39729
Styxit <info@styxit.com>
2018-12-04 21:43:41 +0100

    Solve day1 step 2

--- days/Day1/Solution.php
+++ days/Day1/Solution.php
@@ -6,29 +6,74 @@ use Styxit\AbstractSolution;
 
 class Solution extends AbstractSolution
 {
+    /**
+     * @var int Calculated frequency, starts at 0.
+     */
+    private $frequency = 0;
+
+    /**
+     * @var int[] Keep track of all frequencies found.
+     */
+    private $frequencies = [];
+
+    /**
+     * Find the solution.
+     */
     public function execute()
     {
-        /*
-         * Just sum all lines from the input.
-         * Lines start with `-` or `+` to indicate increase or decrease.
-         */
+        // Calculate frequencies once to get the solution to part 1.
+        $this->calculateFrequency(false);
+        // Set the final number as solution for part 1.
+        $this->part1 = $this->frequency;
 
-        // Start at 0.
-        $frequency = 0;
+        // Keep calculating frequencies until a duplicate has been found.
+        while (true) {
+            $duplicateFrequency = $this->calculateFrequency(true);
 
+            if ($duplicateFrequency !== false) {
+                break;
+            }
+        }
+
+        // A duplicate has been found, this is the solution to part 2.
+        $this->part2 = $duplicateFrequency;
+    }
+
+    /**
+     * Just sum all lines from the input.
+     * Lines start with `-` or `+` to indicate increase or decrease.
+     *
+     * @param bool $returnOnDuplicate When true will return the frequency as soon as the same frequency was already
+     *                                found earlier.
+     *
+     * @return int|false A duplicate value when found, false when no duplicate was found or $returnOnDuplicate was false.
+     */
+    private function calculateFrequency($returnOnDuplicate = false)
+    {
         foreach ($this->input->lines as $line) {
             // First character determines increase or decrease.
             $method = substr($line, 0, 1);
             $number = (int) substr($line, 1);
 
             if ($method == '+') {
-                $frequency += $number;
-            } elseif($method == '-') {
-                $frequency -= $number;
+                $this->frequency += $number;
+            } elseif ($method == '-') {
+                $this->frequency -= $number;
+            }
+
+            /*
+             * Add the new frequency to the list of found frequencies if not already there.
+             * Use array keys instead of values to make search faster.
+             *
+             * Immediately return frequency when looking for duplicates and the found frequency has been found before.
+             */
+            if (!isset($this->frequencies[$this->frequency])) {
+                $this->frequencies[$this->frequency] = true;
+            } elseif ($returnOnDuplicate === true) {
+                return $this->frequency;
             }
         }
 
-        // Set the final number as solution for part 1.
-        $this->part1 = $frequency;
+        return false;
     }
 }
