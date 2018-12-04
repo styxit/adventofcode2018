a5a26d9821e9b3c0f04bb7fb06162ce6ffd0cd51
Styxit <info@styxit.com>
2018-12-04 17:16:35 +0100

    Solve day1 part 1

---
+++ days/Day1/Solution.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Days\Day1;
+
+use Styxit\AbstractSolution;
+
+class Solution extends AbstractSolution
+{
+    public function execute()
+    {
+        /*
+         * Just sum all lines from the input.
+         * Lines start with `-` or `+` to indicate increase or decrease.
+         */
+
+        // Start at 0.
+        $frequency = 0;
+
+        foreach ($this->input->lines as $line) {
+            // First character determines increase or decrease.
+            $method = substr($line, 0, 1);
+            $number = (int) substr($line, 1);
+
+            if ($method == '+') {
+                $frequency += $number;
+            } elseif($method == '-') {
+                $frequency -= $number;
+            }
+        }
+
+        // Return the final number.
+        return $frequency;
+    }
+}
